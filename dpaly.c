690235b32cf008b406cc062bea83c2a30a9339bf
hu xiao <huxiao0816@gmail.com>
2015-07-06 23:24:10 +0800

    update

--- dpaly.c
+++ dpaly.c
@@ -18,3 +18,55 @@
 
 #include <SDL.h>
 #include <SDL_thread.h>
+
+typedef struct MyAVPacketList {
+    AVPacket pkt;
+    struct MyAVPacketList *next;
+    int serial;
+} MyAVPacketList;
+
+typedef struct PacketQueue {
+    MyAVPacketList *first_pkt, *last_pkt;
+    int nb_packets;
+    int size;
+    int abort_request;
+    int serial;
+    SDL_mutex *mutex;
+    SDL_cond *cond;
+} PacketQueue;
+
+typedef struct AudioParams {
+    int freq;
+    int channels;
+    int64_t channel_layout;
+    enum AVSampleFormat fmt;
+    int frame_size;
+    int bytes_per_sec;
+} AudioParams;
+
+typedef struct Clock {
+    double pts;
+    double pts_drift;
+    double last_updated;
+    double speed;
+    int serial;
+    int paused;
+    int *queue_serial;
+} Clock;
+
+typedef struct Frame {
+    AVFrame *frame;
+    AVSubtitle sub;
+    int serial;
+    double pts;
+    double duration;
+    int64_t pos;
+    SDL_Overlay *bmp;
+    int allocated;
+    int reallocate;
+    int width;
+    int height;
+    AVRational sar;
+} Frame;
+
+
